4d826cf17d40d7bafbf06b1f8736fa73c85f1d89
Will Johnson <william.c.johnson@intel.com>
2016-02-16 09:45:40 -0500

    HYD-5461 Update module name from flow-highland to intel-flow-highland
    The flow-highland module needs to be renamed to intel-flow-highland.
    
    This patch will update the module name in the package.json file to
    reflect intel-flow-highland.
    
    - Updated package.json module name
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-#noop, needed for CI
+# There are no tests to run, but CI needs a results file.
+echo '<testsuites><testsuite name="flow-highland" package="flow-highland" timestamp="2014-07-31" id="0" hostname="localhost" tests="0" errors="0" failures="0" skipped="0" time="0.008"></testsuite></testsuites>' > flow-highland-results.xml
+mv flow-highland-results.xml ../results
