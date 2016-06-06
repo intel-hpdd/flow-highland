17ba49c443baccaeba4d6ee7c81ada23898f5865
Joe Grund <joe.grund@intel.com>
2016-06-06 17:14:22 -0400

    HYD-6068 flow-highland: use polymorphic types
    By using polymorphic types in flow-highland we can introspect what a
    stream is containing.
    
    This allows us, and flow greater insight into the values being
    transformed.
    
      - Remove jscs.
      - Change highland to module with inline types.
      - Add basic type assertion tests.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ flow-typed/highland.js
@@ -0,0 +1,45 @@
+// @flow
+
+import type {Writable} from 'stream';
+
+
+declare module highland {
+  declare type nilT = {};
+  declare type IdFn<T> = (xs:T) => any;
+  declare type TransformFn<X,Y> = (xs:X) => Y;
+  declare type pushFn<T> = (err:?Error, val:?T) => void;
+  declare type generatorFn<T> = (push:pushFn<T>, next:() => any) => void;
+  declare class HighlandStream<T> {
+    _destructors:Function[];
+    constructor<Type>(_: void): HighlandStream<Type>;
+    constructor<Type>(_: null): HighlandStream<Type>;
+    constructor<Type>(name:string, emitter:(...rest:any[]) => Type):HighlandStream<Type>;
+    constructor<Type>(xs:Array<Type> | generatorFn<Type> | Promise<Type>):HighlandStream<Type>;
+    write(x:any):boolean;
+    destroy():void;
+    tap<T>(fn:IdFn<T>):HighlandStream<T>;
+    map<R>(fn:TransformFn<T, R>):HighlandStream<R>;
+    filter(fn:(x:T) => mixed):HighlandStream<T>;
+    consume(fn:(err:Error, x:T, push:pushFn<T>, next:Function) => void):HighlandStream<T>;
+    pipe(dest:Writable):Writable;
+    each(fn:(x:T) => any):HighlandStream<nilT>;
+    errors(fn:(err:Error, push:pushFn<T>) => any):HighlandStream<T>;
+    through<R>(fn:(s:HighlandStream<T>) => HighlandStream<R>):HighlandStream<R>;
+    zip<R>(ys:HighlandStream<R>|Array<R>):HighlandStream<R>;
+    pluck(prop:string):HighlandStream<Object>;
+    ratelimit(num:number, ms:number):HighlandStream<T>;
+    scan<R>(memo:R, fn:(memo:R, next:T) => R):HighlandStream<R>;
+    emit(event: string, ...args:Array<any>): boolean;
+    on(event: string, listener: Function): HighlandStreamT<T>;
+    once(event: string, listener: Function): HighlandStreamT<T>;
+  }
+  declare type HighlandStreamT<T> = HighlandStream<T>;
+  declare module.exports: {
+    <Type>(_:void):HighlandStream<Type>;
+    <Type>(_:null):HighlandStream<Type>;
+    <Type>(xs:generatorFn<Type>):HighlandStream<Type>;
+    <Type>(xs:Array<Type> | Promise<Type>):HighlandStream<Type>;
+    <Type>(name:string, emitter:Function):HighlandStream<Type>;
+    nil:nilT;
+  }
+}
