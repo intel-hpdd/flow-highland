ea35f7dfb0e1e5e27e2cd4a18e4836864a9a2418
Joe Grund <grundjoseph@gmail.com>
2017-04-26 11:31:02 -0400

    remove setup-jasmine-env.js + regenerate yarn.lock
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Flow Highland
 
-This is a simple module to host highland flow types. 
+[![Build Status](https://travis-ci.org/intel-hpdd/flow-highland.svg?branch=master)](https://travis-ci.org/intel-hpdd/flow-highland)
+
+This is a simple module to host highland flow types.
--- run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# There are no tests to run, but CI needs a results file.
-echo '<testsuites><testsuite name="flow-highland" package="flow-highland" timestamp="2014-07-31" id="0" hostname="localhost" tests="0" errors="0" failures="0" skipped="0" time="0.008"></testsuite></testsuites>' > flow-highland-results.xml
-mv flow-highland-results.xml ../results
