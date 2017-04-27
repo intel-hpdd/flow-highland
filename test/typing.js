cac14bc54755a777853bc1a7c176e7ff5033813e
Joe Grund <joe.grund@intel.com>
2017-04-27 17:12:58 -0400

    Add type for through with a Duplex stream (#6)
    Fixes #5.
    
    Add a type for through with a Duplex stream.
    
    Also fixup tests and add them + eslint travis check.
    
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- test/typing.js
+++ test/typing.js
@@ -4,11 +4,10 @@ import highland from 'highland';
 
 import type { HighlandStreamT } from 'highland';
 
-// should error.
+// $ExpectError
 const s = highland([1, 2, 3]);
 (s: HighlandStreamT<string>);
 
-//should pass.
 const n = highland([1, 2, 3]).onDestroy(() => {});
 (n: HighlandStreamT<number>);
 
@@ -20,34 +19,29 @@ const n2 = n.map(x => x + 's');
 const n3 = n2.filter(x => x.length > 2);
 (n3: HighlandStreamT<string>);
 
-//should error
 const g = highland(push => {
+  // $ExpectError
   push(null, 1);
 });
 (g: HighlandStreamT<string>);
 
-//should pass
 const g2 = highland(push => {
   push(null, 1);
 });
 (g2: HighlandStreamT<number>);
 
-//should pass
 const s3 = highland([[1, 2, 3]]).flatten();
 
 (s3: HighlandStreamT<number>);
 
-//should pass
 const s4 = highland.map(x => x + 1);
 
 (s4: (xs: HighlandStreamT<number>) => HighlandStreamT<number>);
 
-//should pass
 const s5 = highland.filter(x => x != null);
 
 (s5: (xs: HighlandStreamT<number>) => HighlandStreamT<number>);
 
-// Zip
 const zipper1 = highland([[{ name: 'John Doe' }, { name: 'Jane Doe' }]]).zip([
   [{ id: 1 }, { id: 2 }]
 ]);
